27e447681d59ecb173fd724e7bf748ae809dddda
Wesley Ng <wesleyng2003@gmail.com>
2025-10-17 22:31:09 -0700

    More progress

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,3 +1,7 @@
 extends Node2D
 
 var current_level = 0
+
+func _on_main_menu_start_game() -> void:
+	$UI/Camera2D/CanvasLayer/MainManu.enabled = false
+	$UI/Camera2D/CanvasLayer/GameInterface.enabled = true
---
+++ main_menu.gd
@@ -0,0 +1,9 @@
+extends Control
+
+signal start_game
+
+func _on_start_button_pressed() -> void:
+	start_game.emit()
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit()
